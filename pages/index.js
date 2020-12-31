6b84745525df2c195be89c8a0168a0738b5ae0d4
Nithin Reddy <nithinreddyt@gmail.com>
2020-12-31 15:07:51 +0530

    changed home opage

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <SEO title="What is CRM" />
       <h1> What is CRM ? </h1>
       <p> How to implement an efficient CRM system for your organisaiton ?
-         Check this space to get latest updates on <a className="text-4xl font-bold text-yellow-600 font-display" href= "https://techcentred.com/top-5-best-crm-software-for-every-business/"> best crm softwares</a> Credits to <a href="https://techcentred.com">TechCentred</a></p>
+         Check this space to get latest updates on <a className="text-4xl font-bold text-yellow-600 font-display" href= "https://techcentred.com/top-5-best-crm-software-for-every-business/"> best crm softwares</a>. Credits to <a href="https://techcentred.com">TechCentred</a></p>
     </Layout>
   );
 }
